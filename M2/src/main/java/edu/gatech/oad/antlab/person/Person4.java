a2088c399d1110b0a73e1aff38617934f45c6135
Katherine Pham <kpham39@gatech.edu>
2014-01-22 20:46:07 -0500

    I implemented calc method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 1 put your implementation here
-      return null;
+    	StringBuilder sb = new StringBuilder();
+        for(char c: input.toCharArray()){
+            sb.append(c++);
+        }
+        return sb.toString();
     }
     
     /**
