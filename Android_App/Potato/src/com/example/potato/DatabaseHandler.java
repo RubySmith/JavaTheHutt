5704f7127f0e523fd4ef331242383483ab770093
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2014-03-04 05:04:21 -0500

    set up onCreate() and onUpgrade()

--- Android_App/Potato/src/com/example/potato/DatabaseHandler.java
+++ Android_App/Potato/src/com/example/potato/DatabaseHandler.java
@@ -17,23 +17,30 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String TABLE_PROFILES = "profiles";
     
     // Profiles Table Columns names
-    private static final String KEY_USERNAME = "id";
-    private static final String KEY_PASSWORD = "name";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
     
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     
+    //Creating Tables
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		String CREATE_PROFILES_TABLE = "CREATE TABLE " + TABLE_PROFILES + "("
+                + KEY_USERNAME + " TEXT,"
+                + KEY_PASSWORD + " TEXT" + ")";
+        db.execSQL(CREATE_PROFILES_TABLE);
 	}
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
-	}
+	// Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROFILES);
+ 
+        // Create tables again
+        onCreate(db);
+    }
 
 }
