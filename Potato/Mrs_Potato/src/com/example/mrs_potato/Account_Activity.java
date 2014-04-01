590f8520e6356d6c8e606b4978d54a23f69be5b8
Katherine Pham <kpham39@gatech.edu>
2014-03-31 22:13:47 -0400

    Fixed TextView in Account_Activity

--- Potato/Mrs_Potato/src/com/example/mrs_potato/Account_Activity.java
+++ Potato/Mrs_Potato/src/com/example/mrs_potato/Account_Activity.java
@@ -22,7 +22,7 @@ public class Account_Activity extends Activity {
 	
 	RelativeLayout lView ;
 	private TextView accName;
-	private TextView bal ;
+	private TextView bal;
 	private TextView inter;
 	private TextView accNum;
 	private TextView dn;
@@ -33,45 +33,17 @@ public class Account_Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_account_);
-//		currentAccount = Profile_Activity.getCurrentAccount();
-//		System.out.println("current: " + currentAccount);
-//		accountName = currentAccount.getName();
-//		balance = currentAccount.getBalance();
-//		interest = currentAccount.getInterest();
-//		accountNumber = currentAccount.getAccountNumber();
-//		displayName = currentAccount .getDisplayName();
-//		System.out.println(accountName + ", " + balance);
+		currentAccount = Profile_Activity.getCurrentAccount();
+		balance = currentAccount.getBalance();
 //		//Textviews to show up on xml page
-//		RelativeLayout lView = (RelativeLayout)findViewById(R.id.relativeLayout);
-//		accName = new TextView(this);
-//		accName.setX(130);
-//		accName.setY(50);
-//		bal = new TextView(this);
-//		bal.setX(110);
-//		bal.setY(130);
-////		inter = new TextView(this);
-////		inter.setX(110);
-////		inter.setY(210);
-////		accNum = new TextView(this);
-////		accNum.setX(0);
-////		accNum.setY(290);
-////		dn = new TextView(this);
-////		dn.setX(30);
-////		dn.setY(370);
-//		accName.setText("Account Name: " + accountName);
-//		bal.setText("Current Balance: $" + balance);
-////		inter.setText("Interest: " + interest + "%");
-////		accNum.setText("Account Number: " + accountNumber);
-////		dn.setText("Display Name: " + displayName);
-//		lView.addView(accName);
-//		lView.addView(bal);
-////		lView.addView(inter);
-////		lView.addView(accNum);
-////		lView.addView(dn);
-//		bal=null;
-//		bal = new TextView(this);
-//		bal.setX(110);
-//		bal.setY(130);
+		RelativeLayout lView = (RelativeLayout)findViewById(R.id.relativeLayout);
+		bal = new TextView(this);
+		bal.setX(110);
+		bal.setY(130);
+		bal.setText("Current Balance: $" + balance);
+		lView.addView(bal);
+
+		
 	}
 	@SuppressLint("NewApi")
 	@Override 
@@ -79,57 +51,19 @@ public class Account_Activity extends Activity {
 		super.onResume();
 		RelativeLayout lView = (RelativeLayout)findViewById(R.id.relativeLayout);
 		currentAccount = Profile_Activity.getCurrentAccount();
-		System.out.println("current: " + currentAccount);
 		accountName = currentAccount.getName();
 		balance = currentAccount.getBalance();
 		interest = currentAccount.getInterest();
 		accountNumber = currentAccount.getAccountNumber();
 		displayName = currentAccount .getDisplayName();
-		System.out.println(accountName + ", " + balance);
 		accName = new TextView(this);
 		accName.setX(130);
 		accName.setY(50);
-		bal = new TextView(this);
-		bal.setX(110);
-		bal.setY(130);
-//		bal.addTextChangedListener(new TextWatcher() {
-//		    @Override
-//		    public void afterTextChanged(Editable s) {
-//		        // TODO Auto-generated method stub
-//		    }
-//		    @Override
-//		    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//		        // TODO Auto-generated method stub
-//		    }
-//		    @Override
-//		    public void onTextChanged(CharSequence s, int start, int before, int count) {
-//		        // TODO Auto-generated method stub
-////		        if(Property.GetType() == Property.PROPTYPE.num) {
-////		            float f = Float.parseFloat(s.toString());
-////		            Prop.FromString(f);
-////		        }
-////		        else {
-////		            Prop.FromString(s.toString());
-////		        }
-////		        propertiesToUpdate.add(Prop);
-//		});
-		
 		accName.setText("Account Name: " + accountName);
 		bal.setText("Current Balance: $" + balance);
 		
 		lView.addView(accName);
-		lView.addView(bal);
 	}
-
-//	public void onResume(){
-//		super.onResume();
-//		RelativeLayout lView =(RelativeLayout)findViewById(R.id.relativeLayout);
-//		balance = currentAccount.getBalance();
-//		bal = new TextView(this);
-//		bal.setX(110);
-//		bal.setY(130);
-//		lView.addView(bal);
-//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -141,7 +75,6 @@ public class Account_Activity extends Activity {
 	public void onAddTransaction(View v){
 		Intent intent = new Intent(this, Transaction_Activity.class);
 		startActivity(intent);
-//		finish(); //do not finish because transaction_Activity won't recieve account object
 	}
 	public void onCancel(View v){
 		finish();
